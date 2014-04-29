7e49c2ab4d72b55df25decd6d3c060b8b8b959bb
Hans Kindberg <hans.kindberg@gmail.com>
2014-04-30 01:06:29 +0200

    Code backup

---
+++ HansKindberg.DirectoryServices.AccountManagement.IntegrationTests/PrincipalRepositoryTest.cs
@@ -0,0 +1,29 @@
+﻿using System.DirectoryServices.AccountManagement;
+using HansKindberg.DirectoryServices.AccountManagement.Connections;
+using HansKindberg.DirectoryServices.AccountManagement.QueryFilters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HansKindberg.DirectoryServices.AccountManagement.IntegrationTests
+{
+	[TestClass]
+	public class PrincipalRepositoryTest
+	{
+		#region Methods
+
+		public void GeneralTest()
+		{
+			var principalConnection = new PrincipalConnection {ContextType = ContextType.ApplicationDirectory};
+			var principalRepository = new PrincipalRepository(principalConnection);
+
+			using(var searchResult = principalRepository.Find(new PrincipalQueryFilter()))
+			{
+				foreach(var item in searchResult)
+				{
+					var principal = item;
+				}
+			}
+		}
+
+		#endregion
+	}
+}
